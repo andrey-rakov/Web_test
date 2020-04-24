a2e2c525fc03953810e9e6a3bb4e4aa10c299ff8
Andery Rakov <rakov80@mail.ru>
2020-04-25 00:22:55 +0700

    changing the port

--- app.py
+++ app.py
@@ -394,7 +394,7 @@ def main():
             abort(404)
         return redirect(f'/{0}/{0}')
 
-    port = int(os.environ.get("PORT", 5000)) # 33507
+    port = int(os.environ.get("PORT", 33507)) # 33507
     app.run(host='0.0.0.0', port=port, debug=True)
 
 
